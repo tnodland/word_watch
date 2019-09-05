827cb9ce901c2b3d868578e9c6976071ced8670a
Trevor Nodland <trevor.nodland@gmail.com>
2019-09-05 10:35:21 -0600

    adds functionality for top word appearing on screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import $ from 'jquery'
 
-$(document).ready(() => {
-  // have fun!
-})
+  const wordFetch = fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+
+  wordFetch.then(response => {
+    return response.json()
+    .then(json => {
+      const obj = json.word
+      const topWord = Object.keys(obj)[0];
+      const topCount = Object.values(obj)[0];
+      document.getElementById("word").innerHTML = `Top word: ${topWord} Count: ${topCount}`;
+    });
+  });
